afc92dbf2180cbe8a54504f40712880f88091a85
VovanR <mail@vovanr.com>
2015-02-18 17:01:29 +0300

    Button: Default settings

--- src/scripts/button/button-test.js
+++ src/scripts/button/button-test.js
@@ -1,8 +1,12 @@
 define([
     'chai',
+    'pixi',
+    'resemble',
     'uiButton',
 ], function (
     chai,
+    PIXI,
+    resemble,
     UIButton
 ) {
 
@@ -14,16 +18,94 @@ define([
     describe('UIButton module', function () {
         /**
          */
-        var module = function () {
-            var m = new UIButton();
+        var module = function (o) {
+            var m = new UIButton(o || {});
 
             return m;
         };
+
+        // create an new instance of a pixi stage
+        var stage = new PIXI.Stage(0x66FF99);
+        // create a renderer instance
+        var renderer = PIXI.autoDetectRenderer(220, 220);
+        console.log('Is PIXI.WebGLRenderer', (renderer instanceof PIXI.WebGLRenderer));
+        // add the renderer view element to the DOM
+        document.getElementById('fixtures').appendChild(renderer.view);
+
+        beforeEach(function () {
+            stage.stage.children.forEach(function (item) {
+                stage.removeChild(item);
+            });
+        });
 
         describe('constructor', function () {
             it('should initialize', function () {
                 var m = module();
                 assert.isDefined(m);
+            });
+
+            describe('`position` option', function () {
+                it('should be Object', function () {
+                    var m = module();
+                    assert.isObject(m._position);
+                });
+
+                it('should init values', function () {
+                    var m = module({
+                        position: {
+                            x: 2,
+                            y: 3,
+                        },
+                    });
+                    assert.equal(m._position.x, 2);
+                    assert.equal(m._position.y, 3);
+                });
+
+                it('should have default values', function () {
+                    var m = module();
+                    assert.equal(m._position.x, 0);
+                    assert.equal(m._position.y, 0);
+                });
+            });
+
+            describe('size options', function () {
+                describe('`width` option', function () {
+                    it('should be Number', function () {
+                        var m = module();
+                        assert.isNumber(m._width);
+                    });
+
+                    it('should init value', function () {
+                        var m = module({
+                            width: 150,
+                        });
+                        assert.equal(m._width, 150);
+                    });
+
+                    it('should have default values', function () {
+                        var m = module();
+                        assert.equal(m._width, 100);
+                    });
+                });
+
+                describe('`height` option', function () {
+                    it('should be Number', function () {
+                        var m = module();
+                        assert.isNumber(m._height);
+                    });
+
+                    it('should init value', function () {
+                        var m = module({
+                            height: 20,
+                        });
+                        assert.equal(m._height, 20);
+                    });
+
+                    it('should have default values', function () {
+                        var m = module();
+                        assert.equal(m._height, 100);
+                    });
+                });
             });
         });
     });
--- src/scripts/button/button.js
+++ src/scripts/button/button.js
@@ -3,13 +3,39 @@
  */
 
 define([
+    'lodash',
+    'pixi',
 ], function (
+    _,
+    PIXI
 ) {
+
+    var defaultOptions = {
+        position: {
+            x: 0,
+            y: 0,
+        },
+        width: 100,
+        height: 100,
+    };
 
     /**
      * Button
+     *
+     * @param {Object} [o] Options
+     * @param {Object} [o.position]
+     * @param {Number} [o.position.x=0]
+     * @param {Number} [o.position.y=0]
+     * @param {Number} [o.width=100]
+     * @param {Number} [o.height=10]
      */
     var Button = function (o) {
+        _.defaults(o || {}, defaultOptions);
+
+        this._position = o.position;
+        this._width = o.width;
+        this._height = o.height;
+
         this._init();
     };
 
